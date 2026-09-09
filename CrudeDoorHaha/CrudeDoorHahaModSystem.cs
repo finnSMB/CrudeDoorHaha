f97d5bb501fcaef2a98d2bb36afa94cde05940e2
Finn <100689088+finnSMB@users.noreply.github.com>
2026-09-10 00:09:49 +0200

    intiial commit

---
+++ CrudeDoorHaha/CrudeDoorHahaModSystem.cs
@@ -0,0 +1,33 @@
+using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace CrudeDoorHaha;
+
+public class CrudeDoorHahaModSystem : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        new Harmony(Mod.Info.ModID).PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(BEBehaviorDoor), nameof(BEBehaviorDoor.ToggleDoorState))]
+internal static class CrudeDoorPatch
+{
+    [HarmonyPostfix]
+    private static void PlayLaugh(BEBehaviorDoor __instance)
+    {
+        var world = __instance.Api.World;
+        var pos = __instance.Pos;
+        var air = 0;
+
+        // ln 15: toggleDoorState does also run for opening doors
+        // so this exists to not make the laugh play for doors that did not break
+        // .. and if for some reason there will ever exist more door-crude's (modded, whatever)
+        if (__instance.Api.Side != EnumAppSide.Server || __instance.Block.Code.Path != "door-crude" || world.BlockAccessor.GetBlock(pos).Id != air) return;
+
+        var laughAssLoc = new AssetLocation("crudedoorhaha", $"sounds/laugh{world.Rand.Next(1, 11)}");
+        world.PlaySoundAt(laughAssLoc, pos, 0, null, false, range: 32, volume: 1);
+    }
+}
